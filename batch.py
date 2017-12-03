04531510a37b446fce584efb6447959e48262295
Daehan Kim <really@iamghost.kr>
2017-12-03 17:47:00 +0900

    사진만 재업로드하게 변경, 타임라인 API로 변경

--- batch.py
+++ batch.py
@@ -7,7 +7,7 @@ from tempfile import NamedTemporaryFile
 import arrow
 import requests
 
-from twython import Twython, TwythonError
+from twython import Twython
 from heconvert.converter import e2h
 
 from db import User
@@ -44,7 +44,7 @@ def convert(text):
 
 
 def has_pattern(text):
-    return re.match(pattern, text) is not None
+    return settings.HASH_TAG in text and re.match(pattern, text) is not None
 
 
 def process_tweet(twitter, tweet):
@@ -53,31 +53,22 @@ def process_tweet(twitter, tweet):
     if has_pattern(text):
         media = tweet['extended_entities']['media'][0]
 
-        file_url = media['media_url']
-
-        if media['type'] == 'video':
-            file_url = media['video_info']['variants'][0]['url']
-
-        ext = file_url.split('/')[-1].split('.')[-1]
-
-        if media['type'] == 'photo':
-            file_url += ':orig'
-
         text = text.replace(f"{media['url']}", "").strip()
 
-        with NamedTemporaryFile(suffix=f'.{ext}', delete=False) as f:
-            download_file(file_url, f)
-            f.close()
+        media_id = media['id']
 
-            if media['type'] == 'photo':
+        if media['type'] == 'photo':
+            file_url = media['media_url']
+            ext = file_url.split('/')[-1].split('.')[-1]
+            file_url += ':orig'
+
+            with NamedTemporaryFile(suffix=f'.{ext}', delete=False) as f:
+                download_file(file_url, f)
+                f.close()
                 media_id = twitter.upload_media(media=open(f.name, 'rb'))['media_id']
-            else:
-                media_id = twitter.upload_video(open(f.name, 'rb'), 'video/mp4')['media_id']
+                os.unlink(f.name)
 
-            os.unlink(f.name)
-
-        twitter.update_status(status=convert(text),
-                              media_ids=[media_id])
+        twitter.update_status(status=convert(text), media_ids=[media_id])
         twitter.destroy_status(id=tweet['id'])
         print(f"Processed tweet {tweet['id']}")
 
@@ -92,10 +83,7 @@ def process_user(user):
     twitter = Twython(settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET,
                       user.oauth_token, user.oauth_token_secret)
 
-    twitter_user = twitter.verify_credentials()
-
-    tweets = twitter.search(q=f"from:{twitter_user['screen_name']} #NintendoSwitch", since_id=user.last_tweet_id) \
-        .get('statuses', [])
+    tweets = twitter.get_user_timeline(user_id=user.id)
 
     user.search_limit = int(twitter.get_lastfunction_header('X-Rate-Limit-Remaining', None))
     user.search_limit_reset = arrow.get(twitter.get_lastfunction_header('X-Rate-Limit-Reset', None)) \
@@ -107,8 +95,9 @@ def process_user(user):
     for tweet in tweets:
         try:
             process_tweet(twitter, tweet)
-        except (KeyError, TwythonError):
-            pass
+        except Exception as e:
+            print(e)
+            continue
 
     if len(tweets) > 0:
         user.last_tweet_id = tweets[0]['id']
@@ -119,8 +108,8 @@ def run():
     for user in User.select():
         try:
             process_user(user)
-        except (KeyError, TwythonError):
-            pass
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
