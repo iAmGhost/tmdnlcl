dc476a8c88b7949a7e46ad6036cc8384a86c1735
Daehan Kim <really@iamghost.kr>
2018-01-13 13:30:53 +0900

    검색모드 동시 지원, m3u8 동영상 포함시 예외처리 추가

--- batch.py
+++ batch.py
@@ -1,3 +1,4 @@
+import traceback
 import re
 import shutil
 import os
@@ -56,7 +57,10 @@ def process_tweet(twitter, tweet):
         file_url = media['media_url']
 
         if media['type'] == 'video':
-            file_url = media['video_info']['variants'][0]['url']
+            for variant in media['video_info']['variants']:
+                if variant['content_type'] == 'video/mp4':
+                    file_url = variant['url']
+                    break
 
         ext = file_url.split('/')[-1].split('.')[-1]
 
@@ -66,6 +70,7 @@ def process_tweet(twitter, tweet):
         text = text.replace(f"{media['url']}", "").strip()
 
         with NamedTemporaryFile(suffix=f'.{ext}', delete=False) as f:
+            print(f'Downloading {file_url}')
             download_file(file_url, f)
             f.close()
 
@@ -92,7 +97,11 @@ def process_user(user):
                       user.oauth_token, user.oauth_token_secret)
 
     try:
-        tweets = twitter.get_user_timeline(user_id=user.id, since_id=user.id)
+        if settings.API_MODE == 'search':
+            screen_name = twitter.show_user(user_id=user.id)['screen_name']
+            tweets = twitter.search(q=f"from:{screen_name}")['statuses']
+        else:
+            tweets = twitter.get_user_timeline(user_id=user.id, since_id=user.id)
     except TwythonAuthError:
         db.session.delete(user)
         db.session.commit()
@@ -108,8 +117,8 @@ def process_user(user):
     for tweet in tweets:
         try:
             process_tweet(twitter, tweet)
-        except Exception as e:
-            print(e)
+        except Exception:
+            traceback.print_exc()
 
     if len(tweets) > 0:
         user.last_tweet_id = max(tweets, key=lambda t: t['id'])['id']
@@ -120,8 +129,8 @@ def run():
     for user in db.session.query(User):
         try:
             process_user(user)
-        except Exception as e:
-            print(e)
+        except Exception:
+            traceback.print_exc()
 
 
 if __name__ == '__main__':
