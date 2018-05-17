f276905a6e19cb8d0f0589119f84657a6d0874ab
Daehan Kim <really@iamghost.kr>
2018-05-17 21:57:08 +0900

    테이블명 변경, 통계용 테이블 추가

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
@@ -5,10 +6,27 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.BigInteger, primary_key=True)
     last_tweet_id = db.Column(db.BigInteger, nullable=True)
     oauth_token = db.Column(db.String(255))
     oauth_token_secret = db.Column(db.String(255))
     search_limit = db.Column(db.Integer, nullable=True)
     search_limit_reset = db.Column(db.DateTime, nullable=True)
+
+
+class Stats(db.Model):
+    __tablename__ = 'stats'
+    id = db.Column(db.Integer, primary_key=True)
+    last_update = db.Column(db.DateTime, default=datetime.now)
+
+    @classmethod
+    def get_singleton(cls):
+        instance = db.session.query(cls).first()
+
+        if instance is None:
+            instance = cls()
+            db.session.add(instance)
+            db.session.commit()
+
+        return instance
