71e2d2e16b4ffc971d2e4602abc0691e4d6e8429
Daehan Kim <really@iamghost.kr>
2018-12-14 01:51:14 +0900

    설정파일 오타 수정

--- tmdnlcl/settings.py
+++ tmdnlcl/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = external_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = external_settings.DEBUG
 
-ALLOWED_HOSTS = external_settings.ALLOWED_SETTINGS
+ALLOWED_HOSTS = external_settings.ALLOWED_HOSTS
 
 
 # Application definition
