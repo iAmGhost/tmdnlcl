554b78d947bd40e434fc1bc8e2ac8233e38d4b2d
Daehan Kim <really@iamghost.kr>
2018-12-14 01:58:04 +0900

    Admin 구현

--- tmdnlcl_app/admin.py
+++ tmdnlcl_app/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from solo.admin import SingletonModelAdmin
 
-from tmdnlcl_app.models import AppSetting
+from tmdnlcl_app.models import AppSetting, TwitterUser, Tweet, Attachment
 
 
 @admin.register(AppSetting)
 class AppSettingAdmin(SingletonModelAdmin):
     pass
+
+
+@admin.register(TwitterUser)
+class TwitterUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'rate_limit_remaining', 'rate_limit_reset', 'mode']
+
+
+class AttachmentInline(admin.StackedInline):
+    model = Attachment
+    extra = 1
+
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'submitted_at', 'content']
+    inlines = [AttachmentInline]
